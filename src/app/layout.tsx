1a816694c8839d17e2994afadab1282056216bea
Jesaja Pavlović <jesaja.pavlovic@hva.nl>
2023-03-22 11:56:27 +0100

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import SideNavbar from '@/components/SideNavbar'
 import './globals.css'
 
 export const metadata = {
@@ -12,7 +13,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <SideNavbar/>
+      </body>
+      
     </html>
   )
 }
